c950f52c7143f7b1fb68886584ccf8abfff50dd1
TudorPCT <76231144+TudorPCT@users.noreply.github.com>
2022-11-28 18:50:54 +0200

    Added printing format

--- network_mapper.py
+++ network_mapper.py
@@ -18,12 +18,21 @@ def network_mapper(cidr_class, port_list=None):
     port_regex = re.compile("^((6553[0-5])|(655[0-2][0-9])|(65[0-4][0-9]{2})|"
                             "(6[0-4][0-9]{3})|([1-5][0-9]{4})|([0-5]{0,5})|([0-9]{1,4}))$")
     for ip in ip_list:
+        is_first = True
         for index, port in enumerate(port_list):
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            if type(port) == str:
+                port = int(port)
             if port_regex.match(str(port)) and s.connect_ex((ip, port)) == 0:
-                output = "{}:{}({})".format(ip, port, port_dict[port]) if port in port_dict.keys() \
-                   else "{}:{}".format(ip, port) if index == 0 else str(port).ljust(len(ip))
+                padding = len(ip+str(port)+port_dict.get(port)) + 3 if port_dict.get(port) is not None \
+                    else len(ip+str(port)) + 1
+                output = "{}:{}({})".format(ip, port, port_dict[port]) if port in port_dict.keys() and is_first \
+                    else "{}:{}".format(ip, port) if is_first \
+                    else (":{}({})".format(str(port), port_dict[port])).rjust(padding) if port in port_dict.keys()\
+                    else ":{}".format(str(port)).rjust(padding)
+
                 print(output)
+                is_first = False
             s.close()
 
 
